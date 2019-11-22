9194b0ea9cd81629de9fc52f686f6e48b1775b9d
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2019-11-22 18:39:37 -0500

    name text being added to recording.

--- VideoWithNotes/app/src/main/java/com/hrm123/videowithnotes/VideoRecordingActivity.java
+++ VideoWithNotes/app/src/main/java/com/hrm123/videowithnotes/VideoRecordingActivity.java
@@ -1,21 +1,25 @@
 package com.hrm123.videowithnotes;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.media.CamcorderProfile;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +35,7 @@ import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.TransformableNode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class VideoRecordingActivity extends AppCompatActivity
         implements ModelLoader.ModelLoaderCallbacks {
@@ -48,6 +53,76 @@ public class VideoRecordingActivity extends AppCompatActivity
     // The UI to record.
     private FloatingActionButton recordButton;
 
+    public  boolean isReadStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted1");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked1");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted1");
+            return true;
+        }
+    }
+
+    public  boolean isWriteStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted2");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked2");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted2");
+            return true;
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 2:
+                Log.d(TAG, "External storage2");
+                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+                    //resume tasks needing this permission
+                    // downloadPdfFile();
+                }else{
+                   // progress.dismiss();
+                }
+                break;
+
+            case 3:
+                Log.d(TAG, "External storage1");
+                if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+                    //resume tasks needing this permission
+                    // SharePdfFile();
+                }else{
+                    // progress.dismiss();
+                }
+                break;
+        }
+    }
+
+    private String m_Text = "";
+
     @Override
     @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     // CompletableFuture requires api level 24
@@ -59,24 +134,32 @@ public class VideoRecordingActivity extends AppCompatActivity
             return;
         }
 
+        if(!isReadStoragePermissionGranted()){
+            return;
+        }
+        if(!isWriteStoragePermissionGranted()){
+            return;
+        }
+
         setContentView(R.layout.activity_ux);
         arFragment = (WritingArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
 
         arFragment.setOnTapArPlaneListener(
                 (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    new AlertDialog.Builder(this)
-                            .setTitle("Delete entry")
-                            .setMessage("Are you sure you want to delete this entry?")
-
+                    final EditText input = new EditText(this);
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Enter name")
+                            // .setMessage("Are you sure you want to delete this entry?")
+                            .setView(input)
                             // Specifying a listener allows you to take an action before dismissing the dialog.
                             // The dialog is automatically dismissed when a dialog button is clicked.
                             .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
-                                    // Continue with delete operation
+                                    m_Text = input.getText().toString();
                                 }
                             })
-
                             // A null listener allows the button to dismiss the dialog and take no further action.
                             .setNegativeButton(android.R.string.no, null)
                             .setIcon(android.R.drawable.ic_dialog_alert)
@@ -102,7 +185,7 @@ public class VideoRecordingActivity extends AppCompatActivity
                                     (renderable) -> {
                                         infoCard.setRenderable(renderable);
                                         TextView textView = (TextView) renderable.getView();
-                                        textView.setText("bestandroid1");
+                                        textView.setText("rammohan holagundi");
                                         textView.setTextColor(ColorStateList.valueOf(Color.MAGENTA));
 
                                     })
@@ -192,6 +275,8 @@ public class VideoRecordingActivity extends AppCompatActivity
         }
         return true;
     }
+
+
 
     @Override
     public void setRenderable(ModelRenderable modelRenderable) {
