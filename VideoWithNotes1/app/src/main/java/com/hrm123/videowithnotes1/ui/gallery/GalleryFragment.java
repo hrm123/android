2f5d5d6bdc4463a9fa3ddeac4cddc25203a35612
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2019-11-27 16:55:06 -0500

    initial videlist view

--- VideoWithNotes1/app/src/main/java/com/hrm123/videowithnotes1/ui/gallery/GalleryFragment.java
+++ VideoWithNotes1/app/src/main/java/com/hrm123/videowithnotes1/ui/gallery/GalleryFragment.java
@@ -1,9 +1,12 @@
 package com.hrm123.videowithnotes1.ui.gallery;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,10 +16,19 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.hrm123.videowithnotes1.R;
+import com.hrm123.videowithnotes1.Video;
+import com.hrm123.videowithnotes1.VideoAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    private List<Video> mVideosList = new ArrayList<>();
+    private ListView mListViewVideos;
+    private VideoAdapter mVideoAdapter;
+    private Cursor videocursor;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +42,29 @@ public class GalleryFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+        String selection = MediaStore.Video.Media.DATA +" like?";
+        String[] selectionArgs=new String[]{"%Sceneform%"};
+        String[] parameters = { MediaStore.Video.Media._ID,
+                MediaStore.Video.Media.DATA,
+                MediaStore.Video.Media.DISPLAY_NAME,
+                MediaStore.Video.Media.SIZE };
+        videocursor = getActivity().getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                parameters, selection, selectionArgs, MediaStore.Video.Media.DATE_TAKEN + " DESC");
+        int count = videocursor.getCount();
+        int video_column_index = videocursor
+                .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+        while(videocursor.moveToNext()){
+            String vUrl = videocursor.getString(video_column_index);
+            mVideosList.add(new Video(vUrl));
+        }
+        videocursor.close();
+
+        mListViewVideos  = root.findViewById(R.id.video_list_view);
+        mVideoAdapter = new VideoAdapter(getContext(), mVideosList);
+        mListViewVideos.setAdapter(mVideoAdapter);
+
         return root;
     }
 }
